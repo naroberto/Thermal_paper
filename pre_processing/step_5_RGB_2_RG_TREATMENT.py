6454618bcd47bb9ad3204e3c5f03733538f58500
Robert <rnaudascher@yahoo.de>
2023-02-15 14:48:08 +0100

    Update step_5_RGB_2_RG_TREATMENT.py

--- pre_processing/step_5_RGB_2_RG_TREATMENT.py
+++ pre_processing/step_5_RGB_2_RG_TREATMENT.py
@@ -12,15 +12,8 @@ Output: RED and GREEN channel of each image, time stamped in the file name.
 import cv2
 import os
 import datetime
-#import time
 
-# ---------------   RUN THIS SCRIPT ONLY FOR TREATMENT ---------------
-
-# We should have a distinct final output folder, redo this analog to step_3 !!!
-# directory with rotated and cropped RGB -tifs to be processed 
-
-# path to output file from step 3 -> rethink the path and folder structure!!!
-
+# select experiments
 dates = ['07_07','08_07','09_07']
 runs =  ['run_1','run_2','run_3','run_4','run_5']
 
@@ -40,7 +33,7 @@ for date in dates:
         r_top_path =              os.path.join('G:\Thermal_exp\Final_runs', date, 'Final' , run , 'treat_top_red')
         g_top_path =              os.path.join('G:\Thermal_exp\Final_runs', date, 'Final' , run , 'treat_top_green')
         
-        # SAVE Output
+        # Output folders
         if not os.path.exists(r_side_path):
             os.makedirs(r_side_path)
             
@@ -58,9 +51,9 @@ for date in dates:
         frames_in_part_1 = len(list_part_1)
         list_part_2 = os.listdir(in_dir_top_treat_part2)
         
-        list_all = list_part_1 + list_part_2 # big lsit with all fram names
+        list_all = list_part_1 + list_part_2 # merge lists
             
-        treat_dur = 21*60*24 # constant amount of frames for all treatment periods added on minute as buffer in the end in case we lose soemthing at the beginning
+        treat_dur = 21*60*24      # constant amount of frames for all treatment periods added on minute as buffer
         
         ## Read time file
         with open(time_file) as f:
@@ -76,16 +69,8 @@ for date in dates:
             treat_start_date_time = datetime.datetime.strptime(treat_start_date_time , '%Y-%m-%d %H:%M:%S.%f') # convert to time format
         except ValueError:              # This is to account for the fact that sometimes we have no decimal places in the TIMESTAMP
             treat_start_date_time = datetime.datetime.strptime(treat_start_date_time, '%Y-%m-%d %H:%M:%S')
-        #time_stamp = acclim_start_time + datetime.timedelta(0,count/24)  # 24 fps
-        
-                                             # abstract only the time
-        
-        #acclim_synch_time = datetime(str(contents[14]))  
-        #print(acclim_synch_time)
-        
-        
+
         # LOAD images, split channels, rename them
-        
         # initialize indices
         time_stamp = treat_start_date_time
         count = 0
@@ -104,44 +89,25 @@ for date in dates:
                     img = cv2.imread(os.path.join(in_dir_side_treat_part2, i))     # side CAM!
                     img_top = cv2.imread(os.path.join(in_dir_top_treat_part2, i))  # TOP CAM!
                 
-                # Convert to grayscale
+                # split channels of side cam
                 blue, green, red = cv2.split(img)
-                #cv2.imshow('red', red)
-                #cv2.imshow('green', green)
                 
                 # add 1/24 seconds for this frame, first frame nothing is added...
                 time_stamp = treat_start_date_time + datetime.timedelta(0,count/24)  # 24 fps
                 time_stamp_str = time_stamp.time().strftime("%H_%M_%S_%f") 
                 
-                # insert:
-                # - Convert this time to a string  and add it to the filename below using hh_mm_ss_ffff
-                # - in the end we want to store r_img_00001_14_06_12_034958.tif
+                # store red and green images as .tif
                 cv2.imwrite(os.path.join(r_side_path,"%05d_red_" + time_stamp_str + ".tif") % count, red) # Write the results to output location.
                 cv2.imwrite(os.path.join(g_side_path,"%05d_green_" + time_stamp_str + ".tif") % count, green) # Write the results back to output location.
                 
+                 # split channels of top_cam
                 blue, green_top, red_top = cv2.split(img_top)
                 
                 cv2.imwrite(os.path.join(r_top_path,"%05d_red_" + time_stamp_str + ".tif") % count, red_top) # Write the results to output location.
                 cv2.imwrite(os.path.join(g_top_path,"%05d_green_" + time_stamp_str + ".tif") % count, green_top) # Write the results back to output location.
-                
-                #cv2.waitKey(0)
-                #cv2.destroyAllWindows()
+
                 count = count + 1  # Use all frames
-                #count += skip_frame # skip the frames
                 print("Current frame: " + str(count))
                 if count > 28000:
                     print('More then 20 min. are processed')
                 
-# TOP CAM; ACCLIM        
-
-        
-# THIS IS JUST AN  EXAMPLE
-#str_date = '2021-06-29 14:30:08.916667'
-#d_date = datetime.datetime.strptime(str_date , '%Y-%m-%d %H:%M:%S.%f')
-#print(d_date)
-#d_date_added = d_date + datetime.timedelta(0,216/24) # way to add a delta_time in seconds 
-#print(d_date_added)
-#d_date_added_time = d_date_added.time().isoformat() # abstract only the time
-#print(d_date_added_time)
-#print(type(d_date_added_time)) # check d_date type.
-# END OF EXAMPLE
